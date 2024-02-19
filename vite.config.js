21cdb1698a2cb78861e9fddf6cc4c603e912834c
Jevgeni <j.zolotarjov@gmail.com>
2024-02-19 11:06:37 +0200

    vite page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/portfolio",
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
