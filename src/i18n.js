0d26ca787c222fb03e3322ec1c6adec54029184c
mizolo <mizolo@taltech.ee>
2024-02-19 22:50:34 +0200

    debug

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 i18n
-    .use(LanguageDetector)
+    // .use(LanguageDetector)
     .use(initReactI18next)
     .init({
         // we init with resources
