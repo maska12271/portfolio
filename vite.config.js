ea268b01956f423b1d9a50c824e7ee5664b26b94
maska12271 <135606944+maska12271@users.noreply.github.com>
2024-02-19 22:10:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ssr } from 'vite-plugin-ssr/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ssr({
+      baseAssets: 'https://maska12271.github.io/portfolio/'
+    })],
   base: "/portfolio/"
 })
+
