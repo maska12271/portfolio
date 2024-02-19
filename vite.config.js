aa0e1482b002437300e9ecf2a8017cad1e708658
Jevgeni Zolotarjov <jevgeni.zolotarjov@wisercat.net>
2024-02-19 16:08:35 +0200

    tryfix build

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio",
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
+  base: "/portfolio"
 })
