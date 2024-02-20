768f483c6bcee92b22dc63cc351c34c401feb287
mizolo <mizolo@taltech.ee>
2024-02-20 16:27:14 +0200

    debug UnexcitingPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
                     <Route path="/about" element={<About />}/>
                     <Route path="/contact" element={<Contact />}/>
                 </Route>
-                <Route path="*" element={<UnexcitingPage />}/>
+                <Route path="/*" element={<UnexcitingPage />}/>
             </Routes>
         </BrowserRouter>
     );
